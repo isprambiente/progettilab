9a1b6af5a809b9694e653b2a9ff778ede3dad1ee
francesco.loreti <francesco.loreti@isprambiente.it>
2019-06-19 14:19:01 +0200

    Aggiunto link local

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :application, "progettilab"
 set :repo_url, "git@github.com:isprambiente/progettilab.git"
 set :deploy_to, "/home/progettilab"
 # set :pty, true
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'puma.rb')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'puma.rb', 'settings.local.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/sessions', 'tmp/state', 'vendor/bundle', 'public/system', 'private/files')
 set :tmp_dir, "/home/progettilab/shared/tmp"
 
