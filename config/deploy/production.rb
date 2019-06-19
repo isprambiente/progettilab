ac992c9af49a237b57e169d26918175ab50ad82a
francesco.loreti <francesco.loreti@isprambiente.it>
2019-06-19 11:19:06 +0200

    Corretto parametro deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 role :app, [ "#{Settings.config.application}@"+Rails.application.credentials.deploy[:site_url] ]
 role :web, [ "#{Settings.config.application}@"+Rails.application.credentials.deploy[:site_url] ]
-role :db,  [ "#{Settings.config.application}b@"+Rails.application.credentials.deploy[:site_url] ]
+role :db,  [ "#{Settings.config.application}@"+Rails.application.credentials.deploy[:site_url] ]
 
 server Rails.application.credentials.deploy[:site_url], user: "#{Settings.config.application}", roles: %w{app db web}
 
